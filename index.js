81d506e2b390181270f53f6d7a4510ae5c4908b8
Moringa School <elvis.rinkei@moringaschool.com>
2022-09-27 12:41:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+
 
 /* 
   Add your pseudocode here
 */
+for (let i = 0; i < word.length / 2; i++){
+  const j = word.length -1 -i;
 
 /*
   Add written explanation of your solution here
-*/
+*/if (word[i] !== word[j])
+return false ;}
+return true;
+}
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
